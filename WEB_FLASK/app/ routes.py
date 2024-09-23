28f123cf75e55d3bafa37f2b77db7ab32d8558cf
Arthur2811 <arthur.cadran@gmail.com>
2024-09-23 11:50:08 +0200

    123

--- WEB_FLASK/app/ routes.py
+++
@@ -1,7 +0,0 @@
-from app import app
-
-@app.route('/')  # Décorator pour la page d'accueil
-@app.route('/index')  # Décorator pour la page index
-def index():
-    strResult = 'Hello Bruz!'
-    return strResult
--- WEB_FLASK/app/__init__.py
+++ WEB_FLASK/app/__init__.py
@@ -2,4 +2,5 @@ from flask import Flask
 
 app = Flask(__name__)
 
-from app import routes 
+# Importation des routes après la création de l'objet app
+from app import routes
